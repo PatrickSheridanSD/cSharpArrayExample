55a1a54e19ad032fc90f2b1345f4bdc154d3dae1
patricksheridansd <patrick_sheridan@outlook.com>
2017-02-17 10:37:19 +0000

    c# Array Example
    enter members of a band

---
+++ ArraysBand/Program.cs
@@ -0,0 +1,33 @@
+﻿// foreach loop
+
+using System;
+
+namespace Arrays
+{
+    class Band
+    {
+        private static void Main()
+        {
+            String[] theBeatles = { "John", "Paul", "George", "Ringo" };
+
+            // use a foreach loop to print members of the band
+            foreach (String member in theBeatles)
+            {
+                Console.Write(member + " ");
+            }
+            // foreach limitations:
+            // can't determine index position, cannot change array contents, forward direction only
+
+            Console.WriteLine();
+
+            // create a new reference to the same array
+            String[] theBootlegBeatles = theBeatles;
+
+            foreach (var member in theBootlegBeatles)
+            {
+                Console.Write(member + " ");
+            }
+
+        }
+    }
+}
